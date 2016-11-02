2116e80b391e78fca42aaed51f7b3bd850ed3fd4
Song Yibo <songyb15@mails.tsinghua.edu.cn>
2016-11-02 10:12:42 +0800

    Configuration: Database settings. Url settings.

--- framework/settings.py
+++ framework/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'framework.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'visframe',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'PORT': '3306'
     }
 }
 
