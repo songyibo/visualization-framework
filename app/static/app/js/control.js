bc5910027cd10f5cdba8ba1e4c17c0e9edefd502
Song Yibo <songyb15@mails.tsinghua.edu.cn>
2016-11-09 17:08:15 +0800

    Update: Change main-canvas to svg element. Add foreignObject element for widget rendering.

--- app/static/app/js/control.js
+++ app/static/app/js/control.js
@@ -1,4 +1,4 @@
 var vis = vis || {};
 
-var w1 = new vis.widget.DataSource().init('main-canvas', {x: 200, y: 200});
-var w2 = new vis.widget.Scatterplot().init('main-canvas', {x: 500, y: 100});
+var w1 = new vis.widget.DataSource().init('vis-widget-canvas', {x: 200, y: 200});
+var w2 = new vis.widget.Scatterplot().init('vis-widget-canvas', {x: 500, y: 100});
